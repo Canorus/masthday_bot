43e6c7002e80f476104219253c69c2b7ee06de6e
Canorus <helios7157@gmail.com>
2018-11-26 13:12:20 +0900

    - Change working path - Add created_at workflow

--- birthday_bot.py
+++ birthday_bot.py
@@ -3,53 +3,80 @@
 import json
 import requests
 import csv
-import os
 import datetime
+import pytz
 
 base = '/volume1/homes/canor/scripts/birthday_bot/'
 with open(base+'cred.json') as f:
     cred = json.load(f)
 
 acc = cred[0][1]['cred']['access_token']
-head = {'Authorization':'Bearer '+ acc} # Authorization
-prev = '' # for renewing data everday 
-congs = [] # already congratulated
+head = {'Authorization': 'Bearer ' + acc}  # Authorization
+prev = ''  # for renewing data everday
+congs = []  # already congratulated
+time_format = '%Y-%m-%dT%H:%M:%S' # time format declared for reuse
+instance_address = 'https://twingyeo.kr'
 
-uri_local = 'https://twingyeo.kr/api/v1/streaming/public/local' # connect to local timeline
-r_local = requests.get(uri_local,headers=head,stream=True)
+# connect to local timeline
+uri_local = 'https://twingyeo.kr/api/v1/streaming/public/local'
+r_local = requests.get(uri_local, headers=head, stream=True)
 print('connected..')
 for l in r_local.iter_lines():
     dec = l.decode('utf-8')
     try:
         #print('dec type: '+str(type(dec)))
-        newdec = json.loads(dec.replace('data: ','')) # strip off unnecessary part
+        # strip off unnecessary part
+        newdec = json.loads(dec.replace('data: ', ''))
         try:
             with open(base+'congratulated.txt') as f:
                 for line in f:
-                    congs.append(line.replace('\n','')) # bring already congratulated members
+                    # bring already congratulated members
+                    congs.append(line.replace('\n', ''))
         except:
             congs = []
         account = newdec['account']
-        if account['created_at'][5:10] == datetime.datetime.today().isoformat()[5:10] and account['username'] not in congs: # same created date but not in congs
-            instance = account['url'].split('/')[2] # get instance address
-            n = int(datetime.datetime.today().isoformat()[:4]) - int(account['created_at'][:4]) # calculate how many years have passed
+        # server time is in UTC, local time in KST(Asia/Seoul), converting UTC to KST
+        created_at_server = account['created_at'][:-5]
+        print('created_at_server: '+created_at_server)
+        #time_format = '%Y-%m-%dT%H:%M:%S' # declared outside
+        created_at_server_object = datetime.datetime.strptime(
+            created_at_server, time_format)
+        print('settings timezone')
+        kst_format = pytz.timezone('Asia/Seoul')
+        utc_format = pytz.timezone('UTC')
+        created_at_server_object = utc_format.localize(created_at_server_object)  # now created_at_server has utc timezone
+        print('created_at_server_object')
+        created_at_local = kst_format.normalize(created_at_server_object.astimezone(
+            kst_format)).isoformat()[:-6]  # converted value, last 6 chars are +09:00
+        iso_today = datetime.datetime.today().isoformat()[:-7]
+        print('created_at_local is '+str(created_at_local))
+        print('iso_today is '+str(iso_today))
+        # same created date but not in congs
+        if created_at_local[5:10] == iso_today[5:10] and account['username'] not in congs:
+            instance = account['url'].split('/')[2]  # get instance address
+            # calculate how many years have passed
+            n = int(iso_today[:4]) - int(created_at_local[:4])
             hd = dict()
-            if n > 0: # more than a year
-                hd['status'] = account['display_name']+' 님이 '+instance+'에 가입하신 지 '+str(n)+' 주 년이 되셨습니다. 축하합니다.'
-            elif n == 0: # new member!
-                hd['status'] = account['display_name']+' 님이 '+instance+'에 새로 오셨습니다. 환영합니다.'
-            hd['visibility'] = 'unlisted' # in comply to Twingyeo rules
-            msg = requests.post('https://twingyeo.kr/api/v1/statuses/',headers=head,data=hd)
-            congs.append(account['username']) # add to congs
-            with open(base+'congratulated.txt','w') as f:
+            if n > 0:  # more than a year
+                hd['status'] = account['display_name']+' 님이 ' + \
+                    instance+'에 가입하신 지 '+str(n)+' 주 년이 되셨습니다. 축하합니다.'
+            else:  # new member!
+                hd['status'] = account['display_name'] + \
+                    ' 님이 '+instance+'에 새로 오셨습니다. 환영합니다.'
+            hd['visibility'] = 'unlisted'  # in comply to Twingyeo rules
+            msg = requests.post(
+                'https://twingyeo.kr/api/v1/statuses/', headers=head, data=hd)
+            congs.append(account['username'])  # add to congs
+            with open(base+'congratulated.txt', 'w') as f:
                 for cong in congs:
-                    f.write(cong+'\n') # update already congratulated members
+                    # update already congratulated members
+                    f.write(cong+'\n')
     except:
         if dec == ':thump':
             pass
         elif datetime.datetime.today().day != prev:
-            f = open(base+'congratulated.txt','w')
-            f.close() # new day, new members to congratulate
+            f = open(base+'congratulated.txt', 'w')
+            f.close()  # new day, new members to congratulate
             prev = datetime.datetime.today().day
         else:
-            print(dec)
+            print('error occured: '+dec)
---
+++ created_at.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+import json
+import requests
+import datetime
+import pytz
+import re
+from random import randint
+
+mode = 0
+
+base = '/volume1/homes/canor/scripts/birthday_bot/'
+with open(base+'cred.json') as f:
+    cred = json.load(f)
+
+acc = cred[0][1]['cred']['access_token']
+head = {'Authorization': 'Bearer ' + acc}  # Authorization
+prev = ''  # for renewing data everday
+congs = []  # already congratulated
+time_format = '%Y-%m-%dT%H:%M:%S'  # time format declared for reuse
+instance_address = 'https://twingyeo.kr'
+
+# connect to home timeline
+uri_user = instance_address+'/api/v1/streaming/user'
+r_user = requests.get(uri_user, headers=head, stream=True)
+print('connected..')
+for l in r_user.iter_lines():
+    dec = l.decode('utf-8')
+    if dec == 'event: notification':
+        mode = 1
+    elif dec == 'event: update':
+        mode = 0
+    elif dec == ':thump':
+        mode = 0
+    if mode:  # event: notification
+        try:
+            #print(dec)
+            newdec = json.loads(dec.replace('data: ', ''))
+            if newdec['account']['bot']:
+                raise
+            try:
+                type = newdec['type']
+            except:
+                pass
+            if type == 'mention':
+                reply_to_id = newdec['status']['id']
+                if newdec['account']['display_name']:
+                    username = newdec['account']['display_name']
+                else:
+                    username = newdec['account']['username']
+                reply_to_account = newdec['account']['acct']
+                # if has keyword 계정 + 만들 / 생성
+                if re.search('계정.*((만들)|(만든)|(생성))', newdec['status']['content']) is not None:
+                    # # reply
+                    created_at_server = newdec['account']['created_at'][:-5]
+                    c = datetime.datetime.strptime(created_at_server, time_format)
+                    status = username+' 님의 계정은 세계표준시각 '+str(c.year)+'년 '+str(c.month)+'월 '+str(c.day)+'일 '+str(c.hour)+'시 '+str(c.minute)+'분 '+str(c.second)+'초에 생성되었어요.'
+                else:
+                    status = '죄송합니다. 아직 제가 이해할 수 없는 질문이에요.'
+                    if randint(1, 100) % 3 == 0:
+                        status += '\n하지만 소고기는 자유 소프트웨어가 아니랍니다!'
+                status += ' @'+reply_to_account
+                hd = dict()
+                hd['status'] = status
+                hd['in_reply_to_id'] = reply_to_id
+                hd['visibility'] = 'unlisted'
+                requests.post(instance_address+'/api/v1/statuses', headers=head, data=hd)
+            elif type == 'follow':
+                # follow back
+                pass
+        except:
+            print('exception occured: ')
+            #print(dec)
